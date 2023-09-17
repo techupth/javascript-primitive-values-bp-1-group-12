8992c4a1000661f3f2620f6e51ee19884eeadbe0
Kawin <kawind7@gmail.com>
2023-09-17 22:51:32 +0700

    sent Exercise#1-8 Primitive Values

--- ex-5.js
+++ ex-5.js
@@ -1,3 +1,6 @@
 //Exercise #5: Shopping and Discount
 
 // Start coding here
+let totalPriceBath = 27000
+let discountPrice25Percent = totalPriceBath*(25/100);
+let lastPrice = totalPriceBath - discountPrice25Percent;
